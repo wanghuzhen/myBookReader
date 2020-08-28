4b8cbad90203896bf5c2be8581e7c3337ba94b4e
wanghuzhen <2327253081@qq.com>
2020-08-28 10:45:43 +0800

    second

--- lib/pages/DirPage.dart
+++ lib/pages/DirPage.dart
@@ -9,6 +9,8 @@ class DirPage extends StatefulWidget {
 }
 
 class _DirPageState extends State<DirPage> {
+  bool showToTopBtn = false; //是否显示“返回到顶部”按钮
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -44,55 +46,71 @@ class _DirPageState extends State<DirPage> {
                 );
               },
             ),
-            Align(
-              alignment: Alignment(0.0, 1.0),
-              child: Container(
-                height: 50,
-                decoration: BoxDecoration(color: Colors.white, boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey,
-                    blurRadius: 3.0,
-                  )
-                ]),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Container(
-                        margin: EdgeInsets.only(left: 16.0),
-                        child: Text(
-                          '共' + widget.dirList.length.toString() + '章',
-                          style: TextStyle(
-                            fontSize: 17.0,
-                          ),
-                        ),
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        IconButton(
-                          icon: Icon(Icons.arrow_upward),
-                          onPressed: () {
-                            //TODO 置顶
-                          },
-                          splashColor: Colors.transparent,
-                          highlightColor: Colors.transparent,
-                        ),
-                        IconButton(
-                          splashColor: Colors.transparent,
-                          highlightColor: Colors.transparent,
-                          icon: Icon(Icons.arrow_downward),
-                          onPressed: () {
-                            //TODO 置底
-                          },
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              ),
-            )
+//显示章节数目，或者改用FlatButton
+//            Align(
+//              alignment: Alignment(0.0, 1.0),
+//              child: Container(
+//                height: 50,
+//                decoration: BoxDecoration(color: Colors.white, boxShadow: [
+//                  BoxShadow(
+//                    color: Colors.grey,
+//                    blurRadius: 3.0,
+//                  )
+//                ]),
+//                child: Row(
+//                  children: [
+//                    Expanded(
+//                      child: Container(
+//                        margin: EdgeInsets.only(left: 16.0),
+//                        child: Text(
+//                          '共' + widget.dirList.length.toString() + '章',
+//                          style: TextStyle(
+//                            fontSize: 17.0,
+//                          ),
+//                        ),
+//                      ),
+//                    ),
+//                    Row(
+//                      mainAxisAlignment: MainAxisAlignment.end,
+//                      children: [
+//                        IconButton(
+//                          icon: Icon(Icons.keyboard_arrow_up),
+//                          onPressed: () {
+//                            //TODO 置顶
+//                          },
+//                          splashColor: Colors.transparent,
+//                          highlightColor: Colors.transparent,
+//                        ),
+//                        IconButton(
+//                          splashColor: Colors.transparent,
+//                          highlightColor: Colors.transparent,
+//                          icon: Icon(Icons.keyboard_arrow_down),
+//                          onPressed: () {
+//                            //TODO 置底
+//                          },
+//                        ),
+//                      ],
+//                    )
+//                  ],
+//                ),
+//              ),
+//            )
           ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          elevation: 4.0,
+          onPressed: () {
+            setState(() {
+              showToTopBtn = !showToTopBtn;
+            });
+          },
+          child: Icon(
+            showToTopBtn ? Icons.arrow_upward : Icons.arrow_downward,
+            color: Colors.black,
+          ),
+          backgroundColor: Colors.white,
+          splashColor: Colors.transparent,
+          highlightElevation: 0.0,
         ),
       ),
     );
